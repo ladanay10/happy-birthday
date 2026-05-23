ab88b05dca19ff821fdbbf70bba57d1dca4388c7
ladanay10 <ladanayandriy@gmail.com>
2026-05-23 12:01:40 +0300

    chore: fix date

--- app/t3x/page.tsx
+++ app/t3x/page.tsx
@@ -3,7 +3,7 @@ import InvitePage from "../components/InvitePage";
 export default function Page() {
   return (
     <InvitePage
-      dateDisplay="31 травня 2026"
+      dateDisplay="30 травня 2026"
       timeDisplay="16:00"
       target="2026-05-31T16:00:00"
     />
