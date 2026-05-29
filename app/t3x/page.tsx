6dcf9330a9038c1a19ef40001113b6f023e99064
ladanay10 <ladanayandriy@gmail.com>
2026-05-29 11:17:41 +0300

    chore: small fix

--- app/t3x/page.tsx
+++ app/t3x/page.tsx
@@ -3,9 +3,9 @@ import InvitePage from "../components/InvitePage";
 export default function Page() {
   return (
     <InvitePage
-      dateDisplay="31 травня 2026"
+      dateDisplay="30 травня 2026"
       timeDisplay="16:00"
-      target="2026-05-31T16:00:00+03:00"
+      target="2026-05-30T16:00:00+03:00"
     />
   );
 }
